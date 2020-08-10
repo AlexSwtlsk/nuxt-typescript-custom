f0021ff75d948be9078063f31dc04684ffa2ebc4
Alexandre Switalski <alexandre.swit@gmail.com>
2020-08-10 11:06:44 +0200

    Remove env-shim to let native Nuxt envs take over

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,19 @@
-import { config as dotEnvConfig } from 'dotenv'
 import { head } from './config/head'
-import { envShim } from './config/env-shim'
 import { I18N } from './i18n/i18n'
-
-dotEnvConfig()
 
 export default {
   head,
   mode: 'universal',
 
   loading: { color: '#3B8070' },
+
   buildModules: [
     '@nuxt/typescript-build',
     '@nuxtjs/style-resources',
     ['nuxt-i18n', I18N]
   ],
-  modules: ['@nuxtjs/axios', ['nuxt-env-shim', envShim]],
+
+  modules: ['@nuxtjs/axios'],
 
   styleResources: {
     scss: ['~/assets/scss/main.scss']
@@ -29,5 +27,9 @@ export default {
 
   plugins: [],
 
-  build: {}
+  build: {},
+
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL
+  }
 }
